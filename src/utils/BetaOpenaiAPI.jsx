ab448b28cdfe249c589c80a6d608d451498bc8b2
Zhangyi Wu <glennwuu@outlook.com>
2024-12-18 14:03:42 +0100

    change model

--- src/utils/BetaOpenaiAPI.jsx
+++ src/utils/BetaOpenaiAPI.jsx
@@ -2,98 +2,98 @@ import OpenAI from "openai";
 import getPromptObject from "./PromptObjectGenerator";
 
 export async function authenticateOpenaiAPI() {
-  const apiKey =
-    localStorage.getItem("api-key") === null
-      ? process.env.OAI_KEY
-      : localStorage.getItem("api-key");
-  const model =
-    localStorage.getItem("model") === null
-      ? "gpt-3.5-turbo"
-      : localStorage.getItem("model");
-  const openai = new OpenAI({
-    apiKey: apiKey,
-    dangerouslyAllowBrowser: true,
-  });
-  try {
-    const availableModels = await openai.models.list();
-    return availableModels;
-  } catch (error) {
-    throw error;
-  }
+	const apiKey =
+		localStorage.getItem("api-key") === null
+			? process.env.OAI_KEY
+			: localStorage.getItem("api-key");
+	const model =
+		localStorage.getItem("model") === null
+			? "gpt-3.5-turbo"
+			: localStorage.getItem("model");
+	const openai = new OpenAI({
+		apiKey: apiKey,
+		dangerouslyAllowBrowser: true,
+	});
+	try {
+		const availableModels = await openai.models.list();
+		return availableModels;
+	} catch (error) {
+		throw error;
+	}
 }
 
 export async function getCompletionsFromOpenai(userPrompt, callback) {
-  const apiKey =
-    localStorage.getItem("api-key") === null
-      ? process.env.OAI_KEY
-      : localStorage.getItem("api-key");
+	const apiKey =
+		localStorage.getItem("api-key") === null
+			? process.env.OAI_KEY
+			: localStorage.getItem("api-key");
 
-  const model =
-    localStorage.getItem("model") === null
-      ? "gpt-3.5-turbo"
-      : localStorage.getItem("model");
-  const openai = new OpenAI({
-    apiKey: apiKey,
-    dangerouslyAllowBrowser: true,
-  });
-  const promptObject = getPromptObject(userPrompt);
-  try {
-    const stream = await openai.chat.completions.create({
-      model: model,
-      messages: promptObject,
-      stream: true,
-      temperature: 0.0,
-    });
-    let result = "";
-    for await (const part of stream) {
-      let token = part.choices[0].delta;
-      let content = token?.content;
-      if (token?.content !== undefined) {
-        result += content;
-        callback(result);
-      }
-    }
+	const model =
+		localStorage.getItem("model") === null
+			? "gpt-4o-mini-realtime-preview"
+			: localStorage.getItem("model");
+	const openai = new OpenAI({
+		apiKey: apiKey,
+		dangerouslyAllowBrowser: true,
+	});
+	const promptObject = getPromptObject(userPrompt);
+	try {
+		const stream = await openai.chat.completions.create({
+			model: model,
+			messages: promptObject,
+			stream: true,
+			temperature: 0.0,
+		});
+		let result = "";
+		for await (const part of stream) {
+			let token = part.choices[0].delta;
+			let content = token?.content;
+			if (token?.content !== undefined) {
+				result += content;
+				callback(result);
+			}
+		}
 
-    return result;
-  } catch (error) {
-    if (error instanceof OpenAI.APIError) {
-      console.error(error.status);
-      console.error(error.message);
-      console.error(error.code);
-      console.error(error.type);
-    } else {
-      console.log(error);
-    }
-  }
+		return result;
+	} catch (error) {
+		if (error instanceof OpenAI.APIError) {
+			console.error(error.status);
+			console.error(error.message);
+			console.error(error.code);
+			console.error(error.type);
+		} else {
+			console.log(error);
+		}
+	}
 }
 export async function getInspirationsFromOpenai(inputCode, callback) {
-  const promptObject = getPromptObject(inputCode);
-  try {
-    const stream = await openai.chat.completions.create({
-      model: "gpt-4",
-      messages: promptObject,
-      stream: true,
-      temperature: 0.0,
-    });
-    let result = "";
-    for await (const part of stream) {
-      let token = part.choices[0].delta;
-      let content = token?.content;
-      if (token?.content !== undefined) {
-        result += content;
-        callback(result);
-      }
-    }
+	const promptObject = getPromptObject(inputCode);
+	try {
+		const stream = await openai.chat.completions.create({
+			model: "gpt-4",
+			messages: promptObject,
+			stream: true,
+			temperature: 0.0,
+		});
+		let result = "";
+		for await (const part of stream) {
+			let token = part.choices[0].delta;
+			let content = token?.content;
+			if (token?.content !== undefined) {
+				result += content;
+				callback(result);
+			}
+		}
 
-    return result;
-  } catch (error) {
-    if (error instanceof OpenAI.APIError) {
-      console.error(error.status);
-      console.error(error.message);
-      console.error(error.code);
-      console.error(error.type);
-    } else {
-      console.log(error);
-    }
-  }
+		return result;
+	} catch (error) {
+		if (error instanceof OpenAI.APIError) {
+			console.error(error.status);
+			console.error(error.message);
+			console.error(error.code);
+			console.error(error.type);
+		} else {
+			console.log(error);
+		}
+	}
 }
